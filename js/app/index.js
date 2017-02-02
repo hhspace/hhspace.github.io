28c51ccb9c6f989e5e560a97a76e08e4f1b70276
hhspace <m.mudi@list.ru>
2017-02-02 17:53:44 +0300

    Update index.js

--- js/app/index.js
+++ js/app/index.js
@@ -1,5 +1,5 @@
-const Foo = { template: '<div class="home">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veniam. Ex praesentium quis amet maiores dolor in explicabo maxime pariatur, saepe, nulla enim ea, ipsum laboriosam cupiditate modi ipsa ipsam.</div>' }
-const Bar = {  template: '<div class="contacts">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sint maxime sit et corrupti architecto eius, esse dolor voluptatum vel, illo laboriosam ut distinctio! Ducimus molestiae temporibus explicabo quibusdam impedit?</div>' }
+const Home = { template: '<div class="home">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veniam. Ex praesentium quis amet maiores dolor in explicabo maxime pariatur, saepe, nulla enim ea, ipsum laboriosam cupiditate modi ipsa ipsam.</div>' }
+const Contacts = {  template: '<div class="contacts">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sint maxime sit et corrupti architecto eius, esse dolor voluptatum vel, illo laboriosam ut distinctio! Ducimus molestiae temporibus explicabo quibusdam impedit?</div>' }
  
 
 const routes = [
