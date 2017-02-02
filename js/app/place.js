f08aab9194df70f89c7e5e597fc13352890e9136
hhspace <m.mudi@list.ru>
2017-02-02 17:51:28 +0300

    Update place.js

--- js/app/place.js
+++ js/app/place.js
@@ -1,10 +1,10 @@
-const Foo = { template: '<div class="foo">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veniam. Ex praesentium quis amet maiores dolor in explicabo maxime pariatur, saepe, nulla enim ea, ipsum laboriosam cupiditate modi ipsa ipsam.</div>' }
-const Bar = {  template: '<div class="bar">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sint maxime sit et corrupti architecto eius, esse dolor voluptatum vel, illo laboriosam ut distinctio! Ducimus molestiae temporibus explicabo quibusdam impedit?</div>' }
+const Foo = { template: '<div class="home">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, veniam. Ex praesentium quis amet maiores dolor in explicabo maxime pariatur, saepe, nulla enim ea, ipsum laboriosam cupiditate modi ipsa ipsam.</div>' }
+const Bar = {  template: '<div class="contacts">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore sint maxime sit et corrupti architecto eius, esse dolor voluptatum vel, illo laboriosam ut distinctio! Ducimus molestiae temporibus explicabo quibusdam impedit?</div>' }
  
 
 const routes = [
-  { path: '/foo', component: Foo },
-  { path: '/bar', component: Bar }
+  { path: '/home', component: Home },
+  { path: '/contacts', component: Contacts }
 ]
  
 const router = new VueRouter({
@@ -14,4 +14,4 @@ const router = new VueRouter({
  
 const place = new Vue({
   router
-}).$mount('#place')
+}).$mount('#index')
